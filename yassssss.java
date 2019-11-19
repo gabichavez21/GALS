dac7219b75b2fc2107be43e1905e238eb3106a6a
LaytonEast <57926792+LaytonEast@users.noreply.github.com>
2019-11-18 19:56:46 -0600

    uploading the java file for the team!

---
+++ yassssss.java
@@ -0,0 +1,7 @@
+public class yassssss {
+    public static void main(String args[]){
+       System.out.println("What up people.");
+        System.out.println("This is the best project ever.");
+        System.out.println("Love, Mom");
+    }
+}
