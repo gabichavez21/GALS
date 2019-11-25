51ad967951f030f6c796e1fe82a96c48821182f5
LaytonEast <57926792+LaytonEast@users.noreply.github.com>
2019-11-24 22:47:05 -0600

    Added input validation to Main

--- Scheduler.java
+++ Scheduler.java
@@ -1,30 +1,57 @@
 import java.util.*;
 public class Scheduler {
 
-    // method to create months hashtable
-    public static Hashtable<String, Integer> createMonthsHash(){
+    // method to create monthNumber hashtable
+    public static Hashtable<String, Integer> createMonthNumber(){
         Hashtable<String, Integer>  months  = new Hashtable<String, Integer>();
-        months.put("January", 0);
-        months.put("February", 1);
-        months.put("March", 2);
-        months.put("April", 3);
-        months.put("May", 4);
-        months.put("June", 5);
-        months.put("July", 6);
-        months.put("August", 7);
-        months.put("September", 8);
-        months.put("October", 9);
-        months.put("November", 10);
-        months.put("December", 11);
+        months.put("january", 0);
+        months.put("february", 1);
+        months.put("march", 2);
+        months.put("april", 3);
+        months.put("may", 4);
+        months.put("june", 5);
+        months.put("july", 6);
+        months.put("august", 7);
+        months.put("september", 8);
+        months.put("october", 9);
+        months.put("november", 10);
+        months.put("december", 11);
+
+        return months;
+    }
+
+    // method to create monthDays hashtable
+    public static Hashtable<String, Integer> createMonthDays(){
+        Hashtable<String, Integer>  months  = new Hashtable<String, Integer>();
+        months.put("january", 31);
+        months.put("february", 28);
+        months.put("march", 31);
+        months.put("april", 30);
+        months.put("may", 31);
+        months.put("june", 30);
+        months.put("july", 31);
+        months.put("august", 31);
+        months.put("september", 30);
+        months.put("october", 31);
+        months.put("november", 30);
+        months.put("december", 31);
 
         return months;
     }
     public static void main(String[] args) {
 
-        Hashtable<String, Integer>  months  = createMonthsHash();
+        // used to verify input for month
+        Hashtable<String, Integer>  monthNumber  = createMonthNumber();
 
+        // used to get maximum number of days in a month
+        Hashtable<String, Integer>  monthDays  = createMonthDays();
+        int numDays = 0;
+
+        // get current date information
         GregorianCalendar currentDate = new GregorianCalendar();
+        System.out.println(currentDate);
         int currentDay = currentDate.get(Calendar.DAY_OF_YEAR);
+        int currentYear = currentDate.get(Calendar.YEAR);
 
 	Scanner input = new Scanner(System.in);
 
@@ -37,23 +64,26 @@ public class Scheduler {
 	ArrayList<Task> tasks = new ArrayList();
         
         // string variables for date
-        String strMonth;
-        String strDay;
-        String strYear;
+        String strMonth = "";
+        String strDay = "";
+        String strYear = "";
 
         // int variables for date
-        int month;
-        int day;
-        int year;
+        int month = -1;
+        int day = -1;
+        int year = -1;
 
         // variable to store days remaining til due date
-        int daysRemaining;
+        int daysRemaining = 0;
         int dueDateDay;
 
         String keepGoing = "yes";
+        boolean validInput;
+        boolean validDate;
+        Set<String> keys = monthNumber.keySet();
         int i = -1;
 
-        while ( !keepGoing.equals("*") ) {
+        while ( keepGoing.equals("yes") || keepGoing.equals("y") ) {
             i++;
             // get task name
     	    System.out.println("What task would you like added to your schedule? ");
@@ -68,53 +98,88 @@ public class Scheduler {
 	    System.out.println("Rate this task based on priority from 1-10.");
 	    tasks.get(i).priority = Integer.parseInt(input.nextLine());
 
+            
             // get date input as string
-	    System.out.println("When is this task due by?.");
-	    System.out.println("Please enter Month (e.g. January, February, etc..):" );
-	    strMonth = input.nextLine();
-	    System.out.println("Please enter Day of Month (e.g. 1, 2, 3, etc..):" );
-	    strDay = input.nextLine();
-	    System.out.println("Please enter Year (e.g. 2017, 2018, 2019, etc..):" );
-	    strYear = input.nextLine();
-            System.out.println("Date entered: " + strMonth + " " + strDay + 
-                    ", " + strYear) ;
+            validDate = false;
+            while ( !validDate ) {
+	        System.out.println("When is this task due by?.");
+                validInput = false;
+                while( !validInput ) {
+	            System.out.println("Please enter Month (e.g. January, February, etc..):" );
+	            strMonth = input.nextLine();
+                    strMonth = strMonth.toLowerCase();
 
-            // convert strings to ints
-            Set<String> keys = months.keySet();
-            month = -1;
-            for ( String key : keys ) {
-                if ( key.equals(strMonth) )
-                    month = months.get(key);
-            }
-
-            day = Integer.parseInt(strDay);
-            year = Integer.parseInt(strYear);
-
-            int futureYear = 0;
-            if ( year > currentDate.get(Calendar.YEAR) ) 
-                futureYear = year - currentDate.get(Calendar.YEAR);
-
-            // create gregorian calendar date with due date as params
-            GregorianCalendar dueDate;
-            if ( month != -1 )
-                dueDate = new GregorianCalendar(futureYear, month, day);
-            else
-                throw new IllegalArgumentException("Invalid input for month");
-
-            // find how many days remaining
-            dueDateDay  = dueDate.get(Calendar.DAY_OF_YEAR);
-            System.out.println("futureYear: " + futureYear);
-            System.out.println("year: " + year);
-            //daysRemaining = (dueDateDay - currentDay) + (futureYear * 365);
-            daysRemaining = (dueDateDay - currentDay);
+                    // check input validity, and get month number
+                    month = -1;
+                    for ( String key : keys ) {
+                        if ( key.equals(strMonth) ){
+                            month = monthNumber.get(key);
+                            validInput = true;
+                        }
+                    }
+                    if ( !validInput )
+                        System.out.println("Invalid Input. Must be full name of month.\n");
+                }
                 
+                // get day of month, loop til valid input
+                validInput = false;
+                numDays = monthDays.get(strMonth);
+                while( !validInput ){
+	            System.out.println("Please enter 1-" + numDays + " for Day of Month (e.g. 1, 2, 3, etc..):" );
+	            strDay = input.nextLine();
+                    day = Integer.parseInt(strDay);
+
+                    if ( !(day <= 0 || day > numDays) )
+                        validInput = true;
+                    else
+                        System.out.println("Invalid Input.\n");
+
+                }
+
+
+                validInput = false;
+                while ( !validInput ) {
+	            System.out.println("Please enter Year. Must be >= " + currentYear );
+	            strYear = input.nextLine();
+                    year = Integer.parseInt(strYear);
+
+                    if ( !(year < currentYear) ){
+                        validInput = true;
+                    }
+                    else
+                        System.out.println("Invalid Input. Year must be >= " + currentYear);
+                }
+                System.out.println("Date entered: " + strMonth + " " + strDay + 
+                        ", " + strYear) ;
+
+
+
+                // calculate how many years in the future
+                int futureYear = 0;
+                if ( year > currentYear)  
+                    futureYear = year - currentYear;
+
+                // create gregorian calendar date with due date as params
+                GregorianCalendar dueDate;
+                dueDate = new GregorianCalendar(year, month, day);
+
+                // find how many days remaining
+                dueDateDay  = dueDate.get(Calendar.DAY_OF_YEAR);
+                daysRemaining = (365 * futureYear) + dueDateDay - currentDay;
+
+                if ( daysRemaining < 0 )
+                    System.out.println("\nDue date cannot have already passed.\n\n");
+                else
+                    validDate = true;
+        }
 
             //convert from str to int
             tasks.get(i).days = daysRemaining;
 
             // keep going?
-            System.out.println("Keep Going?");
+            System.out.println("\n\nEnter another task?");
             keepGoing = input.nextLine();
+            keepGoing = keepGoing.toLowerCase();
             System.out.println("\n\n");
 
 	    }
