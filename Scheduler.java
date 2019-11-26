3c423424f2435c6c8d51fbc336b9a2c5cdb6df76
Layton Easterly <lseasterly1@buffs.wtamu.edu>
2019-11-26 14:50:58 -0600

    made Scheduler object oriented, added commenting

--- Scheduler.java
+++ Scheduler.java
@@ -1,93 +1,111 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-public class Scheduler {
+import java.util.*;                     // Scanner and ArrayList
 
-    // method to create monthNumber hashtable
-    public static Hashtable<String, Integer> createMonthNumber(){
-        Hashtable<String, Integer>  months  = new Hashtable<String, Integer>();
-        months.put("january", 0);
-        months.put("february", 1);
-        months.put("march", 2);
-        months.put("april", 3);
-        months.put("may", 4);
-        months.put("june", 5);
-        months.put("july", 6);
-        months.put("august", 7);
-        months.put("september", 8);
-        months.put("october", 9);
-        months.put("november", 10);
-        months.put("december", 11);
+import java.io.*;                       // Used for FileWriter
+                                        // and BufferedReader
 
-        return months;
+public class Scheduler{
+
+    private int[] schedule;             // Array of ordered indices that form
+                                        // a schedule of taskList
+    private ArrayList<Task> tasks;      // ArrayList of task objects to 
+                                        // manage tasks
+
+    // create a new schedule 
+    public Scheduler(){
+
+        tasks = new ArrayList<Task>();
     }
 
-    // method to create monthDays hashtable
-    public static Hashtable<String, Integer> createMonthDays(){
-        Hashtable<String, Integer>  months  = new Hashtable<String, Integer>();
-        months.put("january", 31);
-        months.put("february", 28);
-        months.put("march", 31);
-        months.put("april", 30);
-        months.put("may", 31);
-        months.put("june", 30);
-        months.put("july", 31);
-        months.put("august", 31);
-        months.put("september", 30);
-        months.put("october", 31);
-        months.put("november", 30);
-        months.put("december", 31);
+    // add a task to the task list 
+    public void addTask(){
 
-        return months;
-    }
-  
-  	//Method for creating a schedule of tasks using the shortest job first greedy algorithm
-	//preconditions: takes in an array of class tasks that have been given by the user
-	//				 assumes no task will take over 9999 minutes
-	//Postcondition: Outputs an array of indexes depending on the order of the tasks to be completed
-	public static int[] SJF(ArrayList<Task> tasks) {
-		int i, j = 0; //index
-		//first fill a new array with all the values of time in tasks
-		int len = tasks.size();
-		int[] times = new int[len];
-		for(i = 0; i < len; i++) {
-			times[i] = tasks.get(i).time;
-		}
-		
-		//Find the job that takes the shortest amount of time first
-		//If two or more jobs have the same time then the algorithm defaults to FIFO
-		int smallest = 9999;
-		int smallind = -1; //index for smallest value
-		int indexes[] = new int[len];
-		while (j < len) { //fills array with indexes depending on task time in a increasing order
-			for(i = 0; i < len; i++) { //checks all the values to find the next smallest one
-				if(times[i] <= smallest && times[i] >= 0) {	//finds smallest must be positive
-					smallest = times[i];
-					smallind = i;
-				} //end if
-			} //end for
-			indexes[j] = smallind;
-			times[smallind] = -1; //sets the smallest values to -1 so not picked again
-			smallest = 9999; //resets smallest to test on
-			j++;
-		} //end while
-		
-		return indexes;
-	} //end SJF
+        tasks.add( new Task() );
         
-        //Method for creating a schedule of tasks using the First Come First Serve algorithm
-        //Pre: takes in an array of class Tasks that have been given by the user
-        //Post: returns an array of indexes depending on the input order
-        public static int[] FCFS(ArrayList<Task> tasks) {
-            int len = tasks.size();
-            int indexes[] = new int[len];
-            for(int i = 0; i < len; i++){
-                indexes[i] = tasks.indexOf(tasks.get(i));//returning index of list in order it was put in
-            }
-            return indexes ;
-        }//end FCFS
+    }
+   
+    // retrieve a task
+    public Task getTask(int index){
+
+        return tasks.get(index);
+    }
+
+    // number of tasks in task list
+    public int size(){
+
+        return this.tasks.size();
+    }
+
+/*===================================================================================
+        SCHEDULING ALGORITHMS
+=====================================================================================*/
+
+    
+  //Method for creating a schedule of tasks using the shortest job first greedy algorithm
+  //preconditions: takes in an array of class tasks that have been given by the user
+  //				 assumes no task will take over 9999 minutes
+  //Postcondition: Outputs an array of indexes depending on the order of the tasks to be completed
+  public int[] SJF() {
+  	int i, j = 0; //index
+  	//first fill a new array with all the values of time in tasks
+  	int len = tasks.size();
+  	int[] times = new int[len];
+  	for(i = 0; i < len; i++) {
+  		times[i] = this.getTask(i).time;
+  	}
+  	
+  	//Find the job that takes the shortest amount of time first
+  	//If two or more jobs have the same time then the algorithm defaults to FIFO
+  	int smallest = 9999;
+  	int smallind = -1; //index for smallest value
+  	int indexes[] = new int[len];
+  	while (j < len) { //fills array with indexes depending on task time in a increasing order
+  		for(i = 0; i < len; i++) { //checks all the values to find the next smallest one
+  			if(times[i] <= smallest && times[i] >= 0) {	//finds smallest must be positive
+  				smallest = times[i];
+  				smallind = i;
+  			} //end if
+  		} //end for
+  		indexes[j] = smallind;
+  		times[smallind] = -1; //sets the smallest values to -1 so not picked again
+  		smallest = 9999; //resets smallest to test on
+  		j++;
+  	} //end while
+  	
+  	return indexes;
+  } //end SJF
+  
+
+
+
+
+
+
+
+  //Method for creating a schedule of tasks using the First Come First Serve algorithm
+  //Pre: takes in an array of class Tasks that have been given by the user
+  //Post: returns an array of indexes depending on the input order
+  public int[] FCFS() {
+      int len = tasks.size();
+      int indexes[] = new int[len];
+      for(int i = 0; i < len; i++){
+          indexes[i] = tasks.indexOf(this.getTask(i));//returning index of list in order it was put in
+      }
+      return indexes ;
+  }//end FCFS
+
+
+
+
+
+
+
+
 
     public static void main(String[] args) {
+        
+/*===================================================================================
+        INITIAL SETUP FOR MAIN
+=====================================================================================*/
 
         // used to verify input for month
         Hashtable<String, Integer>  monthNumber  = createMonthNumber();
@@ -96,22 +114,23 @@ public class Scheduler {
         Hashtable<String, Integer>  monthDays  = createMonthDays();
         int numDays = 0;
 
+
+
         // get current date information
         GregorianCalendar currentDate = new GregorianCalendar();
         //System.out.println(currentDate);
         int currentDay = currentDate.get(Calendar.DAY_OF_YEAR);
         int currentYear = currentDate.get(Calendar.YEAR);
 
+
+        // initialize scanner object for user input
 	Scanner input = new Scanner(System.in);
 
+
+        // print title
 	System.out.println("Scheduling Program");
 	System.out.println(" ");
 
-	//System.out.println("How many tasks do you need to complete?");
-	//int t = input.nextInt();
-        @SuppressWarnings({"unchecked", "rawtypes"})
-	ArrayList<Task> tasks = new ArrayList();
-        
         // string variables for date
         String strMonth = "";
         String strDay = "";
@@ -126,29 +145,49 @@ public class Scheduler {
         int daysRemaining = 0;
         int dueDateDay;
 
+        // variables to control loops and validate input
         String keepGoing = "yes";
         boolean validInput;
         boolean validDate;
         Set<String> keys = monthNumber.keySet();
         int i = -1;
 
+
+
+/*===================================================================================
+        GET USER INPUT
+=====================================================================================*/
+
+
+
+        // create Schedule object
+        Scheduler sched = new Scheduler();
+
         while ( keepGoing.equals("yes") || keepGoing.equals("y") ) {
             i++;
             // get task name
     	    System.out.println("What task would you like added to your schedule? ");
-            tasks.add(new Task());
-	    tasks.get(i).title = input.nextLine();
+            sched.addTask();
+            Task currentTask = sched.getTask(i);
+	    currentTask.title = input.nextLine();
             
             // get task length
 	    System.out.println("How long will this task take in minutes? ");
-	    tasks.get(i).time = Integer.parseInt(input.nextLine());
+	    currentTask.time = Integer.parseInt(input.nextLine());
 
             // get task priority
 	    System.out.println("Rate this task based on priority from 1-10.");
-	    tasks.get(i).priority = Integer.parseInt(input.nextLine());
+	    currentTask.priority = Integer.parseInt(input.nextLine());
 
             
-            // get date input as string, loop til valid
+            // Below logic gets due date from user and validates
+            // it against the current date pulled from the calendar.
+            //
+            // Date must be entered as:         > full month name
+            //                                  > day of month
+            //                                  > year
+            //
+            // Date must also be current date or future date
             validDate = false;
             while ( !validDate ) {
 	        System.out.println("When is this task due by?.");
@@ -199,9 +238,11 @@ public class Scheduler {
                     else
                         System.out.println("Invalid Input. Year must be >= " + currentYear);
                 }
+
+
+
                 System.out.println("Date entered: " + strMonth + " " + strDay + 
                         ", " + strYear) ;
-
 
 
                 // calculate how many years in the future
@@ -224,8 +265,11 @@ public class Scheduler {
                     validDate = true;
         }
 
+
+
+
             //convert from str to int
-            tasks.get(i).days = daysRemaining;
+            currentTask.days = daysRemaining;
 
             // keep going?
             System.out.println("\n\nEnter another task?");
@@ -236,8 +280,11 @@ public class Scheduler {
 	    }
 
 
+
+
+
         // print all tasks, used for debugging
-        for ( Task task : tasks ) {
+        for ( Task task : sched.tasks ) {
             System.out.println(task.title);
             System.out.println(task.time);
             System.out.println(task.priority);
@@ -246,45 +293,90 @@ public class Scheduler {
         
         System.out.println("\n\n");
 
+
+/*===================================================================================
+        CREATE SCHEDULES AND TESTING
+=====================================================================================*/
         // get number of tasks
-        int numTasks = tasks.size();
+        int numTasks = sched.tasks.size();
 
-        // array to hold array indices in tasks
-        // from shortest job to longest job
-        int[] indexes = new int[numTasks];
         
-        // sort array indices
-        long startTime = System.nanoTime();
-        indexes = SJF( tasks );
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime);
-        long seconds = (endTime - startTime)/1000000000;
+        // create schedule using SJF
+        // and time it
+        int[] SJF_sched = new int[numTasks];
+        Test test1 = new Test();
+        test1.start();
+        SJF_sched = sched.SJF( );
+        test1.stop();
+        test1.display();
+        test1.write("SJF_times.txt");
+
+
+        // create schedule using FCFS_times
+        // and time it
+        int[] FCFS_times = new int[numTasks];
+        Test test2 = new Test();
+        test2.start();
+        FCFS_times = sched.FCFS( );
+        test2.stop();
+        test2.display();
+        test2.write("FCFS_times.txt");
+
         // checking validity of sorted indices
+        /*
         for ( int index : indexes ){
-            System.out.println(tasks.get(index).title);
-            System.out.println(tasks.get(index).time);
+            System.out.println(sched.tasks.get(index).title);
+            System.out.println(sched.tasks.get(index).time);
         }
-
-        // duration of SJF
-        System.out.println("\n\nSJF Function Time in nano seconds: \n\n" + duration);
-        System.out.println("\n\nSJF Function Time roughly in seconds: \n\n" + seconds);
-
-        String durationString = Long.toString(duration);
-
-        // creates the file
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("SJF_times.txt", true));
-            writer.append(durationString + "\n");
-             
-            writer.close();
-        }
-        catch ( IOException e ){
-            System.out.println(e.toString());
-            System.out.println("could not create file SJF_times.txt.");
-        }
-
+        */
+         
     }
 
+
+/*===================================================================================
+        SUPPORTING CLASSES AND METHODS
+=====================================================================================*/
+
+    // method to create monthNumber hashtable
+    public static Hashtable<String, Integer> createMonthNumber(){
+        Hashtable<String, Integer>  months  = new Hashtable<String, Integer>();
+        months.put("january", 0);
+        months.put("february", 1);
+        months.put("march", 2);
+        months.put("april", 3);
+        months.put("may", 4);
+        months.put("june", 5);
+        months.put("july", 6);
+        months.put("august", 7);
+        months.put("september", 8);
+        months.put("october", 9);
+        months.put("november", 10);
+        months.put("december", 11);
+
+        return months;
+    }
+
+    // method to create monthDays hashtable
+    public static Hashtable<String, Integer> createMonthDays(){
+        Hashtable<String, Integer>  months  = new Hashtable<String, Integer>();
+        months.put("january", 31);
+        months.put("february", 28);
+        months.put("march", 31);
+        months.put("april", 30);
+        months.put("may", 31);
+        months.put("june", 30);
+        months.put("july", 31);
+        months.put("august", 31);
+        months.put("september", 30);
+        months.put("october", 31);
+        months.put("november", 30);
+        months.put("december", 31);
+
+        return months;
+    }
+
+    // helper class used to simulate a struct
+    // used to keep track of each task's details
     private static class Task {
         public int days;
         public int priority;
@@ -298,4 +390,66 @@ public class Scheduler {
             title = "";
         }
     }
-}
+
+
+    // helper class used for timing each
+    // scheduling algorithm's completion time
+    private static class Test {
+
+        private long time;      // start time of test in nanoseconds
+        private long duration;  // completion time of test in nanoseconds
+        private long seconds;   // completion time in seconds 
+        private String durationString;  // String format of completion time for
+                                        // writing to the file
+
+
+        // initialize a new test
+        public Test(){
+            time = 0;
+            duration = 0;
+            seconds = 0;
+            durationString = "";
+        }
+
+        // start timer
+        public void start(){
+
+            time = System.nanoTime();
+        }
+
+        // stop timer and make conversions
+        public void stop(){
+
+            long stopTime = System.nanoTime();
+
+            duration = stopTime - time;
+            durationString = Long.toString(duration);
+            seconds = duration / 1000000000;
+        }
+
+        // display completion time to console
+        public void display(){
+
+            System.out.println("\n\nSJF Function Time in nano seconds: \n\n" + duration);
+            System.out.println("\n\nSJF Function Time roughly in seconds: \n\n" + seconds);
+        }
+
+        // create txt file of times
+        public void write(String fileName){
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
+                writer.append(durationString + "\n");
+                 
+                writer.close();
+            }
+            catch ( IOException e ){
+                System.out.println(e.toString());
+                System.out.println("could not create file " + fileName + ".");
+            }
+        }
+
+
+    }
+
+}   
+
